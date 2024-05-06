de7df9444ef4d23fd9c93c51828e2a5da8f1dcce
jonas07br <jonasrafa07@gmail.com>
2024-05-06 14:01:35 -0300

    update biblioteca

--- CatalogoDeLivros/CatalogoLivros/src/Biblioteca.java
+++ CatalogoDeLivros/CatalogoLivros/src/Biblioteca.java
@@ -1,5 +1,10 @@
 public class Biblioteca {
     public static void main(String[] args) {
-        
+        Catalogo lista = new Catalogo();
+        System.out.println(" a");
+        lista.adicionarLivro("As Cronicas De Narnia", "Jonas", 2001);
+        lista.adicionarLivro("As Cronicas De Narnia 2", "Jonas", 2003);
+        lista.adicionarLivro("As Cronicas De Narnia 3", "Jonas", 2006);
+        lista.pesquisaAutor("Jonas");
     }
 }
