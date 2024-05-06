82393326a65e8008f2d8b62e3075536e40b5adde
jonas07br <jonasrafa07@gmail.com>
2024-05-06 12:22:20 -0300

    Update

--- src/CarrinhoDeCompras.java
+++ src/CarrinhoDeCompras.java
@@ -2,8 +2,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CarrinhoDeCompras {
-    public static void main(String[] args) throws Exception {
-        
-        
+    public String nomeItem;
+    public double valor;
+    public int qtd;
+
+    public CarrinhoDeCompras(String nomeItem, double valor, int qtd) {
+        this.nomeItem = nomeItem;
+        this.valor = valor;
+        this.qtd = qtd;
     }
+
+    public String getNomeItem() {
+        return nomeItem;
+    }
+
+   
+    
 }
---
+++ src/Mercado.java
@@ -0,0 +1,19 @@
+public class Mercado {
+    public static void main(String[] args) {        
+        Compras compra = new Compras();
+        compra.adicionarCompra("Leite", 10, 1);
+        compra.adicionarCompra("Carne", 40, 1);
+        
+        compra.removerItem("Leite");
+        
+        compra.adicionarCompra("Pão", 3, 1);
+        compra.adicionarCompra("Suco",5,4);
+        compra.adicionarCompra("Queijo", 10, 3);
+        
+        compra.removerItem("Pão");
+        compra.removerItem("Suco");
+        compra.removerItem("Queijo");
+        compra.exibirItens();
+        System.out.println("Total:"+compra.calcularValorTotal());
+    }
+}
