29ff18f8b851f8f9d25428bad45a93b0a62468c4
jonas07br <jonasrafa07@gmail.com>
2024-05-06 13:19:00 -0300

    Update Mercado

--- src/Mercado.java
+++ src/Mercado.java
@@ -1,19 +1,22 @@
+import java.util.ConcurrentModificationException;
+
 public class Mercado {
     public static void main(String[] args) {        
         CarrinhoDeCompras compra = new CarrinhoDeCompras();
-        compra.adicionarCompra("Leite", 10, 1);
-        compra.adicionarCompra("Carne", 40, 1);
-        
-        compra.removerItem("Leite");
-        
-        compra.adicionarCompra("Pão", 3, 1);
-        compra.adicionarCompra("Suco",5,4);
-        compra.adicionarCompra("Queijo", 10, 3);
-        
-        compra.removerItem("Pão");
-        compra.removerItem("Suco");
-        compra.removerItem("Queijo");
+       
+        compra.adicionarCompra("Laranja",15,1);
+        compra.adicionarCompra("Feijão", 10, 5);
+        compra.adicionarCompra("Arroz", 7, 5);
+        compra.adicionarCompra("Tomate", 6, 3);
         compra.exibirItens();
-        System.out.println("Total:"+compra.calcularValorTotal());
+        System.out.println("O total é:"+compra.calcularValorTotal());
+        compra.removerItem("Laranja");
+        compra.removerItem("Arroz");
+        compra.exibirItens();
+        System.out.println("O total é:"+compra.calcularValorTotal());
+        
+        
+        
+        
     }
 }
